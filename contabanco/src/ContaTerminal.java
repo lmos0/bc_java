f7796c75ecbe3a88599f68c6b65f4ee5f1ba0fe6
lmos0 <125509344+lmos0@users.noreply.github.com>
2023-08-25 13:48:57 -0300

    1

--- contabanco/src/ContaTerminal.java
+++ contabanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    Scanner scanner = new Scanner(System.in);
+
+    int numero;
+    String agencia;
+    String NomeCliente;
+    float saldo  = 875.40f;
+
+    System.out.println("Digite o número da sua conta:");
+    numero = scanner.nextInt();
+
+    System.out.println("Digite o número da sua agência");
+    agencia = scanner.next();
+
+    System.out.println("Digite seu nome:");
+    NomeCliente = scanner.next();
+
+    System.out.println("Olá " + NomeCliente + ", obrigado por ter uma conta conosco. Sua agência é " + agencia + ", conta de número " + numero + ". Seu saldo de " + saldo + " já está disponível para resgaste.");
+    
+
     }
 }

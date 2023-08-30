6f54e5090037e9f6f97d8485d19f627463ff302e
lmos0 <125509344+lmos0@users.noreply.github.com>
2023-08-30 17:48:56 -0300

    exercicio

--- Processo/src/ProcessoSeletivo.java
+++ Processo/src/ProcessoSeletivo.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
@@ -8,8 +9,10 @@ public class ProcessoSeletivo {
         int candidatoAtual = 0;
         double salarioBase = 2000.0;
 
-        imprimirSelecionados();
-
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+        
         while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
             String candidato = candidatos[candidatoAtual];
             double salarioPretendido = valorPretendido();
@@ -23,27 +26,31 @@ public class ProcessoSeletivo {
         }
     }
 
-    static void imprimirSelecionados(){
-        String[] candidatos = {"Mônica", "Fabrício", "Juliana", "Fernanda", "Lucas"};
-        for(int indice = 0; indice < candidatos.length; indice++){
-            System.out.println("O candidato de nº " + (indice +1) + " é o " + candidatos[indice] );
-        }
+    static void entrandoEmContato(String candidato) {
+        int TentativaRealizada = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) {
+                TentativaRealizada++;
+            }
+        } while (continuarTentando && TentativaRealizada < 3);
+
+        if (atendeu) {
+            System.out.println("Conseguiu contato com " + candidato + " na tentativa " + TentativaRealizada);}
+            else {
+                System.out.println("Não conseguiu contato com " + candidato + ". Número máximo de tentativas: " + TentativaRealizada);
+            }
+    }  
+
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
     }
 
     static double valorPretendido() {
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
-
-
-    static void analisarCandidatura(double salarioPretendido) {
-        double salarioBase = 2000.0;
-        if (salarioBase > salarioPretendido) {
-            System.out.println("Ligar para o candidato");
-        } else if (salarioBase == salarioPretendido) {
-            System.out.println("Ligar para o candidato com contra-proposta");
-        } else {
-            System.out.println("Ligar para os demais candidatos");
-        }
-    }
 }
-
